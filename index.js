0ba275721ad92290f4da96bb409b76d10168fefb
valeeriiiee <obleavalerie@gmail.com>
2024-09-01 23:25:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,31 @@ import {
   removeContact,
   addContact,
 } from "./contacts";
+
+import { Command } from "commander";
+const program = new Command();
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
